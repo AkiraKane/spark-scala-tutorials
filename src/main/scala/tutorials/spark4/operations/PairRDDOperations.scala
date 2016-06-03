586235b307c0cc6ae068e6dc9206c167b8d3ee2d
leeivan <leeivan2008@gmail.com>
2016-06-03 08:35:05 +0800

    add

--- src/main/scala/tutorials/spark4/operations/PairRDDOperations.scala
+++ src/main/scala/tutorials/spark4/operations/PairRDDOperations.scala
@@ -1,7 +1,7 @@
 package tutorials.spark4.operations
 
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.{HashPartitioner, SparkConf, SparkContext}
 
 /**
   * Created by leeivan on 4/10/16.
@@ -14,8 +14,8 @@ object PairRDDOperations {
     val appName = "Test Spark basic functions of transformations and actions"
     val conf = new SparkConf().setMaster("local").setAppName(appName)
     val sc = new SparkContext(conf)
-    val rdd = sc.parallelize(List(('B', 1), ('B', 2), ('A', 3), ('A', 4), ('A', 5), ('D', 8)))
-    val other = sc.parallelize(('B', 9) ::('A', 10) ::('A', 3) :: Nil)
+    val rdd = sc.parallelize(List(('B', 1), ('B', 2), ('A', 3), ('A', 4), ('A', 5), ('B', 1)))
+    val other = sc.parallelize(('B', 9) ::('A', 10) ::('D', 3) :: Nil)
     val result01 = rdd.reduceByKey((x, y) => x + y)
     val result02 = rdd.groupByKey()
     val result03 = rdd.mapValues(x => x + 1)
@@ -31,6 +31,9 @@ object PairRDDOperations {
     val result13 = rdd.countByKey()
     val result14 = rdd.countByValue()
     val result15 = rdd.filter { case (key, value) => value < 4 }
+    val result15_1 = rdd.filter { case (key, value) => key == 'B' }.distinct()
+    val result15_2 = rdd.collectAsMap()
+    val result15_3 = rdd.lookup('A')
 
     println("result01: " + result01.collect.mkString(" "))
     println("result02: " + result02.collect.mkString(" "))
@@ -47,6 +50,10 @@ object PairRDDOperations {
     println("result13: " + result13.mkString("   "))
     println("result14: " + result14.mkString("   "))
     println("result15: " + result15.collect.mkString(" "))
+    println("result15_1: " + result15_1.collect.mkString(" "))
+    println("result15_2: " + result15_2.mkString(" "))
+    println("result15_3: " + result15_3.mkString(" "))
+
 
     val deptEmployees = List(
       ("cs", ("jack", 1000.0)),
@@ -80,5 +87,12 @@ object PairRDDOperations {
     println("result20: " + result20.collect.mkString(" "))
     val result21 = result20.map { case (key, value) => (key, value._1 / value._2.toFloat) }
     println("result21: " + result21.collect.mkString(" "))
+
+    val pairs01 = sc.parallelize(List((1, 1), (2, 2), (3, 3)))
+    val result22 = pairs01.partitioner.getOrElse(0)
+    println("result22: " + result22)
+    val pairs02 = pairs.partitionBy(new HashPartitioner(100))
+    val result23 = pairs02.partitioner.get
+    println("result23: " + result23)
   }
 }
