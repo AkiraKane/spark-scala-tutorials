5351ada6b350a3ac10892ce0f4c7ff6c669a0fc6
leeivan <leeivan2008@gmail.com>
2016-06-23 09:31:38 +0800

    add

--- build.sbt
+++ build.sbt
@@ -2,27 +2,28 @@ name := "spark-scala-tutorials"
 
 version := "1.0"
 
-scalaVersion := "2.11.8"
+scalaVersion := "2.10.5"
 
-libraryDependencies ++= Seq("org.apache.spark" % "spark-core_2.11" % "1.6.1",
+libraryDependencies ++= Seq("org.apache.spark" % "spark-core_2.10" % "1.6.1",
   "org.scala-lang" % "scala-compiler" % scalaVersion.value,
   "org.scala-lang" % "scala-library" % scalaVersion.value,
   "org.scala-lang" % "scala-reflect" % scalaVersion.value,
   "org.apache.commons" % "commons-lang3" % "3.3.2",
   "org.slf4j" % "slf4j-api" % "1.7.10",
-  "org.apache.spark" % "spark-sql_2.11" % "1.6.1",
-  "org.apache.spark" % "spark-streaming_2.11" % "1.6.1",
-  "org.apache.spark" % "spark-mllib_2.11" % "1.6.1",
+  "org.apache.spark" % "spark-sql_2.10" % "1.6.1",
+  "org.apache.spark" % "spark-streaming_2.10" % "1.6.1",
+  "org.apache.spark" % "spark-mllib_2.10" % "1.6.1",
   "log4j" % "log4j" % "1.2.17",
   "net.sf.opencsv" % "opencsv" % "2.3",
   "org.scalactic" %% "scalactic" % "2.2.6",
   "org.scalatest" %% "scalatest" % "2.2.6" % "test",
-  "org.apache.spark" % "spark-streaming-twitter_2.11" % "1.6.1" exclude("org.apache.spark", "spark-streaming_2.11"),
-  "org.apache.spark" % "spark-streaming-kafka_2.11" % "1.6.1" exclude("org.apache.spark", "spark-streaming_2.11"),
-  "org.apache.spark" % "spark-streaming-flume_2.11" % "1.6.1" exclude("org.apache.spark", "spark-streaming_2.11"),
-  "org.apache.spark" % "spark-hive_2.11" % "1.6.1",
-  "com.typesafe.play" % "play-json_2.11" % "2.4.6",
-  "com.esotericsoftware" % "kryo" % "3.0.3").map(
+  "org.apache.spark" % "spark-streaming-twitter_2.10" % "1.6.1" exclude("org.apache.spark", "spark-streaming_2.10"),
+  "org.apache.spark" % "spark-streaming-kafka_2.10" % "1.6.1" exclude("org.apache.spark", "spark-streaming_2.10"),
+  "org.apache.spark" % "spark-streaming-flume_2.10" % "1.6.1" exclude("org.apache.spark", "spark-streaming_2.10"),
+  "org.apache.spark" % "spark-hive_2.10" % "1.6.1",
+  "com.typesafe.play" % "play-json_2.10" % "2.4.6",
+  "com.esotericsoftware" % "kryo" % "3.0.3",
+  "org.apache.avro" % "avro-mapred" % "1.8.1").map(
   _.excludeAll(ExclusionRule(organization = "org.mortbay.jetty")))
 
 
---
+++ src/main/scala/com/pinecone/tutorials/spark4basic/fileFormat/json/BasicParseJsonWithJackson.scala
@@ -0,0 +1,62 @@
+package com.pinecone.tutorials.spark4basic.fileFormat.json
+
+import com.fasterxml.jackson.databind.{DeserializationFeature, ObjectMapper}
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.{SparkConf, SparkContext}
+import play.api.libs.json._
+
+
+/**
+  * Created by ivan on 2016/6/22.
+  */
+case class Person(name: String, lovesPandas: Boolean)
+
+// Note: must be a top level class
+object BasicParseJsonWithJackson {
+  Logger.getLogger("org").setLevel(Level.OFF)
+  Logger.getLogger("akka").setLevel(Level.OFF)
+  System.setProperty("hadoop.home.dir", "C:\\winutils")
+
+  def main(args: Array[String]) {
+    val inputFile = "files/pandainfo.json"
+    val conf = new SparkConf().setMaster("local").setAppName("BasicParseJsonWithJackson")
+    val sc = new SparkContext(conf)
+    val input = sc.textFile(inputFile)
+
+    // Parse it into a specific case class. We use mapPartitions beacuse:
+    // (a) ObjectMapper is not serializable so we either create a singleton object encapsulating ObjectMapper
+    //     on the driver and have to send data back to the driver to go through the singleton object.
+    //     Alternatively we can let each node create its own ObjectMapper but that's expensive in a map
+    // (b) To solve for creating an ObjectMapper on each node without being too expensive we create one per
+    //     partition with mapPartitions. Solves serialization and object creation performance hit.
+    val rddParseJsonWithJackson = input.mapPartitions(records => {
+      // mapper object created on each executor node
+      val mapper = new ObjectMapper with ScalaObjectMapper
+      mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+      mapper.registerModule(DefaultScalaModule)
+      // We use flatMap to handle errors
+      // by returning an empty list (None) if we encounter an issue and a
+      // list with one element if everything is ok (Some(_)).
+      records.flatMap(record => {
+        try {
+          Some(mapper.readValue(record, classOf[Person]))
+        } catch {
+          case e: Exception => None
+        }
+      })
+    }, true)
+    val result01 = rddParseJsonWithJackson.filter(_.lovesPandas).mapPartitions(records => {
+      val mapper = new ObjectMapper with ScalaObjectMapper
+      mapper.registerModule(DefaultScalaModule)
+      records.map(mapper.writeValueAsString(_))
+    })
+    println("result01" + result01.collect().mkString(" "))
+    implicit val personReads = Json.format[Person]
+    val rddParseJson = input.map(Json.parse(_))
+    val result02 = rddParseJson.flatMap(record => personReads.reads(record).asOpt)
+    result02.filter(_.lovesPandas).map(Json.toJson(_))
+    println("result02" + result02.collect().mkString(" "))
+  }
+}
